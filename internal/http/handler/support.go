ba35e378dccbc73c2a14727ea7ece4ba437214c7
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2024-03-21 02:26:22 +0300

    Updated postform

--- internal/http/handler/support.go
+++ internal/http/handler/support.go
@@ -30,9 +30,7 @@ func (h *Handler) SendToSupport(c *gin.Context) {
 	email := c.PostForm("mail")
 	title := c.PostForm("title")
 	message := c.PostForm("message")
-	createdAt := c.PostForm("timestamp")
-
-	fmt.Println("created at", createdAt)
+	createdAt := c.PostForm("createdAt")
 
 	createdTime, err := time.Parse(time.RFC3339, createdAt)
 	if err != nil {
